4ac116f64a17cf4ba251bfdacb5855867762ed27
chrish25 <chrish22@gmx.de>
2013-11-30 13:02:14 +0100

    add message templates

---
+++ lunchy/client/views/messages.js
@@ -0,0 +1,17 @@
+messagesData = [
+    {
+        message: 'heute Büffee',
+        author: 'cgr'
+    },
+    {
+        message: 'nein',
+        author: 'nll'
+    },
+    {
+        message: 'doch',
+        author: 'cba'
+    }
+];
+Template.messages.helpers({
+    messages: messagesData
+});
