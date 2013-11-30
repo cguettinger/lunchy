df31b646d27c3fe7549bcd7a994ed786310897ff
Andreas Schmidt <andreas.schmidt@sybit.de>
2013-11-30 15:48:27 +0100

    subtract myGroups from allGroups

--- lunchy/client/views/groupList.js
+++ lunchy/client/views/groupList.js
@@ -12,6 +12,24 @@ Meteor.subscribe('groups');
 Template.groupList.helpers({
     myGroups: myGroups,
     allGroups: function () {
+        allGroupsWithoutMyGroups = [];
+        allGroups = Groups.find().fetch();
+        _.forEach(allGroups, function(group, i, allGroups){
+            insert = true;
+            myGroups.forEach(function(myGroup) {
+                if(myGroup.name == group.name){
+                    insert = false;
+                }
+            });
+            /*_.forEach(myGroups, function(mygroup, i, myGroups){
+                if(mygroup.name == group.name){
+                    insert = false;
+                }
+            });*/
+            if(insert){
+                //allGroupsWithoutMyGroups.add(group);
+            }
+        });
         return Groups.find();
     }
 });
