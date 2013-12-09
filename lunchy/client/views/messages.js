3505cba262293c42730401efc4b9785a9e029b0d
chrish25 <chrish22@gmx.de>
2013-12-09 11:43:47 +0100

    messages nach (minuten) gruppiert

--- lunchy/client/views/messages.js
+++ lunchy/client/views/messages.js
@@ -1,16 +1,39 @@
 Meteor.subscribe("messages");
 
+var previousMessagesDate = null;
 Template.messagesItems.helpers({
+
     messages: function () {
-        return Messages.find({}).fetch().reverse()
+        return Messages.find({}, {sort: {timestamp: -1}});
+    },
+    dateChanged: function (timestamp) {
+        var date = new Date(timestamp);
+        var returnValue = false;
+        if (previousMessagesDate != null && !(previousMessagesDate.getYear() == date.getYear() && previousMessagesDate.getMonth() == date.getMonth() && previousMessagesDate.getDay() == date.getDay() && previousMessagesDate.getMinutes() == date.getMinutes())) {
+            returnValue = true;
+        }
+        previousMessagesDate = date;
+        return returnValue;
+    },
+
+    formattedDate: function (timestamp) {
+        var date = new Date(timestamp);
+        return date.getDay() + "." + (date.getMonth() + 1) + "." + date.getFullYear() + "  " + date.getMinutes();
     }
+
 });
+
+
 
 Template.messages.events({
     'submit #add_message': function (evt) {
         evt.preventDefault();
-        var userName = Meteor.user().emails[0].address.split("@")[0];
-        Messages.insert({message: $(evt.target).find("input").val(), author: userName});
+        var message = {};
+        message.message = $(evt.target).find("input").val();
+        message.author = Meteor.user().emails[0].address.split("@")[0];
+        message.timestamp = new Date().getTime();
+
+        Messages.insert(message);
         $(evt.target).find("input").val("");
     }
 });
