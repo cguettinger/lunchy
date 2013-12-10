dbcdb3dc9ef0e8df870bf525775430d8552f4105
Christian Guettinger <cgr@sybit.de>
2013-12-10 13:50:15 +0100

    anlegen von test gruppen entfernt

--- lunchy/server/server.js
+++ lunchy/server/server.js
@@ -3,25 +3,6 @@
  */
 
 
-if (Groups.find().count() === 0) {
-    var groupBr = Groups.insert({
-        name: "BR"
-    });
-
-    var groupMedia = Groups.insert({
-        name: "Media"
-    });
-
-    var groupEcom = Groups.insert({
-        name: "E-Commerce"
-    });
-
-    var group3oben = Groups.insert({
-        name: "3-oben"
-    });
-
-
-}
 checkInsertAllowed = function(userId, doc){ return !! userId; }
 
 Meteor.publish('messages', function(limit) {
